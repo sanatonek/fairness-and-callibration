8582a37c575b2fb4dacb5d9bd6c334567bfcd935
shuja.khalid <s.khalid@surgicalsafety.com>
2019-11-25 18:19:18 -0500

    Fixed training sequence

--- multicalib/train.py
+++ multicalib/train.py
@@ -4,47 +4,45 @@ import argparse
 import numpy as np
 import pandas as pd
 from torch.utils.data import DataLoader
-from multicalib.utils import train_predictor
-from multicalib.models import IncomeDataset, CreditDataset, NNetPredictor
+from utils import train_predictor
+from models import IncomeDataset, CreditDataset, NNetPredictor
 
 sys.path.append('..')
 
 def train(args):
-    def __init__(self, args):
-        self.args = args
-        self.model = []
+    args = args
+    model = []
 
-        # Load the datasets
-        if (self.args.data == 'income'):
-            trainset = IncomeDataset(file='adult_train.npz', root_dir=self.args.path+'data/')
-            trainloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True)
-            testset = IncomeDataset(file='adult_test.npz', root_dir=self.args.path+'data/')
-            testloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True)
-        elif (self.args.data == 'credit'):
-            trainset = CreditDataset(file='credit_card_default_train.xls', root_dir=self.args.path+'data/')
-            trainloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True)
-            testset = CreditDataset(file='credit_card_default_test.xls', root_dir=self.args.path+'data/')
-            testloader = DataLoader(trainset, batch_size=self.args.batch_size, shuffle=True)
+    # Load the datasets
+    if (args.data == 'income'):
+        trainset = IncomeDataset(file='adult_train.npz', root_dir=args.path+'data/')
+        trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
+        testset = IncomeDataset(file='adult_test.npz', root_dir=args.path+'data/')
+        testloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
+    elif (args.data == 'credit'):
+        trainset = CreditDataset(file='credit_card_default_train.xls', root_dir=args.path+'data/')
+        trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
+        testset = CreditDataset(file='credit_card_default_test.xls', root_dir=args.path+'data/')
+        testloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
 
-        # Train a predictor model
-        if (self.args.mode == 'train'):
-            model = NNetPredictor(trainset.__dim__())
-            train_predictor(model, trainloader, epochs=self.args.epochs)
-            torch.save(model, self.args.path+'models/checkpoint_'+self.args.data+'.mdl')
-            torch.save(model.state_dict(), self.args.path+'models/checkpoint_'+self.args.data+'.pth')
-        else:
-            model = torch.load(self.args.path+'models/checkpoint_'+self.args.data+'.mdl')
-            model.load_state_dict(torch.load(self.args.path+'models/checkpoint_'+self.args.data+'.pth'))
+    # Train a predictor model
+    model = NNetPredictor(trainset.__dim__())
+    train_predictor(model, trainloader, epochs=args.epochs)
+    torch.save(model, args.path+'models/checkpoint_'+args.data+'.mdl')
+    torch.save(model.state_dict(), args.path+'models/checkpoint_'+args.data+'.pth')
 
-        self.model = model
-        self.trainset = trainset
-        self.testset = testset
+
+    model = model
+    trainset = trainset
+    testset = testset
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Use multicalibration to report clibrated results of a model')
     parser.add_argument('--batch_size', type=int, default=100, help='Training batch size')
     parser.add_argument('--epochs', type=int, default=80, help='Training epochs')
+    parser.add_argument('--data', type=str, default='income', help='Training epochs')
+    parser.add_argument('--path', type=str, default='./', help='Training epochs')
     parser.add_argument('--train',  action='store_true', help='Train the predictor model first')
 
     args = parser.parse_args()
-    main(args)
+    train(args)
